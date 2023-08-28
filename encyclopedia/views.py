6f4267a5175a9c1bd5e998cd8c5e030f7ab3e83b
Marwanishere <marwan.hasan.work@gmail.com>
2023-08-28 17:00:43 +0100

    finally got the entry page fully sorted out

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,19 +9,23 @@ def index(request):
 
 def get(request):
     title = request.POST['title']
-    # following seven lines that allow for a title to be dictated 
+    # following six lines that allow for a title to be dictated 
     # were made with the assitance of cs50.ai chatbot
     # I also learned how to use visual studio's python debugger
     # in the process
     markdown_text = util.get_entry(title)
     if markdown_text != None:
         lines = markdown_text.split('\n')
-        title = title.lstrip('# ')
+        title = title.lstrip('# '),
         title = lines[0] if lines else None
         markdown_text = markdown_text.lstrip(title)
+        return render (request, "encyclopedia/get.html", {
+            "title": title.lstrip('# '),
+            "get": markdown_text
+        })
     else :
         title = None
-    return render (request, "encyclopedia/get.html", {
-        "title": title,
-        "get": markdown_text
-    })
+        return render (request, "encyclopedia/get.html", {
+            "title": title,
+            "get": markdown_text
+        })
