ca1d73e5dc0d501ddc5070d9d2d8efa2a031678d
Marwanishere <marwan.hasan.work@gmail.com>
2023-08-28 16:43:02 +0100

    finally got the entry page fully sorted out

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -10,6 +9,17 @@ def index(request):
 
 def get(request):
     title = request.POST['title']
+    # following six lines that allow for a title to be dictated 
+    # were made with the assitance of cs50.ai chatbot
+    # I also learned how to use visual studio's python debugger
+    # in the process
+    markdown_text = util.get_entry(title)
+    if markdown_text != None:
+        lines = markdown_text.split('\n')
+        title = lines[0] if lines else None
+    else :
+        title = None
     return render (request, "encyclopedia/get.html", {
-        "get": util.get_entry(title)
+        "title": title.lstrip('# '),
+        "get": markdown_text.lstrip(title)
     })
